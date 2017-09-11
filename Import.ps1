58a11d566f9bb431d65a551e658b7420843c0f7e
Dominic Z <dominic.zanardi@gmail.com>
2017-09-10 19:50:18 -0600

    Pagination
    Paginated uploads due to null / overload errors during HP upload process.

--- Import.ps1
+++ Import.ps1
@@ -1,29 +1,51 @@
-# HP Connected-MX | AD Bulk User Import
+# HP Connect-MX Import Script
 #
-# This script is designed to pull designated Active Directory group membership & attributes,
-# and transform those results into an HP CMX-compatible CSV file for automated Bulk User Import.
+# This script is used to pull designated Active Directory group membership & attributes,
+# and transform them into an HP CMX-compatible CSV file for import.
 #
-# Place into a scheduled task to automatically sync AD Security group memberships with your HP Connected-MX User base.
+#
 #
 
-$masterADGroup=''
-$masterHPGroupID=''
+$masterADGroup=""
+$masterHPGroupID=""
+$uploadCap=50
+$numPages=0
+$masterWorkingDirectory=""
 
-# Get all AD members of group "", then pull the required LDAP attributes, export into CSV file
-Get-ADGroupMember $masterADGroup -recursive | % {
+# Get member count from security group, divide by 100, save result for later pagination
+$groupMemberCount = (Get-ADGroupMember $masterADGroup).count
+$numPages = [math]::Round($groupMemberCount / $uploadCap)
+
+# Get all AD members of masterADGroup, then pull the attributes, export into CSV file
+echo "Generating array from $masterADGroup ..."
+$userArray=Get-ADGroupMember $masterADGroup -recursive | % {
 	$group=$_
-	Get-ADuser $_ -Properties * | select GivenName,sn,telephoneNumber,employeeID,mail
-} | Export-Csv -Path .\members.csv -NoTypeInformation
+	Get-ADuser $_ -Properties * | Select GivenName,sn,email,telephoneNumber,employeeID
+} 
 
-# Import same CSV file and add HP CMX required columns for GroupId, GroupName, Status, Comments, Notification, collabOptOut 
-$list = Import-Csv .\members.csv 
-$list | Select-Object @{ expression={$_.GivenName}; label='UserFirstName' }, @{ expression={$_.sn}; label='UserLastName' }`
-,@{ expression={$_.mail}; label='UserEmailAddress' },@{ expression={$_.telephoneNumber}; label='PhoneNumber' }`
-,@{ expression={$_.employeeID}; label='federatedUniqueId' },@{Name='GroupId';Expression={$masterHPGroupID} }`
-,@{Name='GroupName';Expression={NULL}},@{Name='Status';Expression={'Active'}},@{Name='Comments';Expression={NULL}}`
-,@{Name='Notification';Expression={NULL}},@{Name='collabOptOut';Expression={NULL}}`
- | Export-Csv -Path .\members.csv -NoTypeInformation
+#paginate by number of pages set in numPages above
+For ($i=0; $i -lt $numPages; $i++) {
+    
+    $iplusone = $i + 1
+    echo "Generating page: $iplusone of $numpages"
+    $arrayMin=$iplusone*$uploadCap
+    $arrayMax=$arrayMin+$uploadCap
 
-#call HP CMX Upload process
-$HPcommand = 'java -jar importusers.jar -csvfile .\members.csv -emailaddress '' -servername cmx-us.connected.com -port 443 -outputdir .\'
-iex $HPcommand
+    $userArray[$arrayMin..$arrayMax] | Export-Csv -Path .\groupmembers.csv -NoTypeInformation
+
+    #sleep to allow for time to save CSV file
+    sleep 2
+
+    #import same CSV file and add HP CMX required columns for GroupId, GroupName, Status, Comments, Notification, collabOptOut 
+    echo "Converting page: $iplusone of $numpages"
+    $list = Import-Csv .\groupmembers.csv 
+    $list | Select-Object @{ expression={$_.GivenName}; label='UserFirstName' }, @{ expression={$_.sn}; label='UserLastName' },@{ expression={$_.email}; label='UserEmailAddress' },@{ expression={$_.telephoneNumber}; label='PhoneNumber' },@{ expression={$_.employeeID}; label='federatedUniqueId' },@{Name='GroupId';Expression={$masterHPGroupID} },@{Name='GroupName';Expression={NULL}},@{Name='Status';Expression={'Active'}},@{Name='Comments';Expression={NULL}},@{Name='Notification';Expression={NULL}},@{Name='collabOptOut';Expression={NULL}}`
+     | Export-Csv -Path .\groupmembers.csv -NoTypeInformation
+
+    #call HP CMX Upload process
+    echo "Uploading page: $iplusone of $numpages"
+    echo "HP Cloud response: "
+    $HPcommand = 'java -jar importusers.jar -csvfile .\groupmembers.csv -preservelogin -emailaddress <> -servername cmx-us.connected.com -port 443 -outputdir .\'
+    iex $HPcommand
+
+}
